a6ec65fe73cf80b8b75cbfe2db183625d7492675
David Callaghan <david.callaghan@hotmail.co.uk>
2025-09-18 21:03:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ const returnSpeed = 0.05;
 
 // --- Load GLTF Model ---
 const loader = new GLTFLoader();
-loader.load('https://davidcallaghan-droid.github.io/threejsstudies/webrevamp1.glb', (gltf) => {
+loader.load('./threejsstudies/webrevamp1.glb', (gltf) => {
   const model = gltf.scene;
   scene.add(model);
 
